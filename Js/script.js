aaef4627bdef63513c84dcb8412217296dacebaf
sejaljais09 <sejaljaiswal716@gmail.com>
2026-03-24 22:14:40 +0530

    commit day 5.1

--- Js/script.js
+++ Js/script.js
@@ -16,5 +16,12 @@ for(let i=0;i<totalNavList;i++){
         navList[j].querySelector("a").classList.remove("active");
     }
    this.classList.add("active")
+   showSection(this);
   })
+}
+function showSection(element){
+    const target=element.getAttribute("href").split("#")[1];
+    document.querySelector("#",target).classList.add("active")
+    
+    
 }
