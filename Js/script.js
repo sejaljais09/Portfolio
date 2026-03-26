3fbfa885ef3ebcac3e906c83242b0312c143c0d7
sejaljais09 <sejaljaiswal716@gmail.com>
2026-03-26 17:21:39 +0530

    commit day 5.1

--- Js/script.js
+++ Js/script.js
@@ -15,17 +15,20 @@ for(let i=0;i<totalNavList;i++){
   const a=navList[i].querySelector("a");
   a.addEventListener("click",function()
   {
-     for(let i=0;i<totalSection;i++){
-        allSection[i].classList.remove("back-section");
-    }
+     
+    removeBackSection();
     for(let j=0;j<totalNavList;j++){
         if(navList[j].querySelector("a").classList.contains("active")){
-          allSection[j].classList.add("back-section");
+        //   allSection[j].classList.add("back-section");
+        addBackSection();
         }
         navList[j].querySelector("a").classList.remove("active");
     }
    this.classList.add("active")
    showSection(this);
+   if(window.innerWidth<1200){
+    asideSectionTogglerBtn();
+   }
   })
 }
 function showSection(element){
@@ -37,6 +40,35 @@ function showSection(element){
     
     
 }
+function removeBackSection(){
+    for(let i=0;i<totalSection;i++){
+        allSection[i].classList.remove("back-section");
+    }
+
+}
+function addBackSection(num){
+    allSection[num].classList.add("back-section");
+}
+
+function updateNav(element){
+    for(let i=0;i<totalNavList;i++){
+        navList[i].querySelector("a").classList.remove("active");
+        const target=element.getAttribute("href").split("#")[1];
+        if(target===navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+                    navList[i].querySelector("a").classList.add("active");
+        }
+    }
+   
+    
+}
+document.querySelector(".hire-me").addEventListener("click",function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    // console.log(sectionIndex);
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
 const navTogglerBtn=document.querySelector(".nav-toggler"),
 aside=document.querySelector(".aside");
 navTogglerBtn.addEventListener("click",()=>{
